2a472055e203faa749a0e53bdabcefd7de421fa0
MOHAMED FARIS K <farischappanangadi95@gmail.com>
2023-09-04 17:44:15 +0530

    fibnocci added

---
+++ fibnocci/fib.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int* fib(int );
+void main(){
+    int n;
+    printf("Enter an input number: ");
+    scanf("%d",&n);
+    int *output=fib(n);
+    printf("Fibonacci series upto %d is ",n);
+    for(int i=0;output[i]!=-1;i++){
+        printf("%d ",output[i]);
+    }
+    return;
+}
+
+int *fib(int n){
+    static int series[100];
+    int a=0,b=1,c;
+    int count=0;
+    count++;
+    series[count]=b;
+    while(a+b<n){
+        c=a+b;
+        count=count+1;
+        series[count]=c;
+        a=b;
+        b=c;
+
+    }
+    series[count+1]=-1;
+    return series;
+}
