81d01def82863d9a713c9e0229c51addf76ae5f0
ANORA\samsunny <samsunny@anorasolutions.com>
2023-09-05 09:18:42 +0530

    q3 of matrix questions modified

--- matrix qstn3/matrixquestion_3_sam.c
+++ matrix qstn3/matrixquestion_3_sam.c
@@ -1,16 +1,19 @@
 /*
 Write a program that takes a 2D array as input 
-and returns the sum of all the odd row index and even column index elements using a function.*/
+and returns the sum of all the odd row index and even column index elements using a function.
+*/
+
 #include<stdio.h>
-int sum(int (*matrix)[]);
+int sum(int,int,int (*matrix)[]);
 int main ()
 {
     int result=0,rows,cols;
     printf("enter no.of rows :");
     scanf("%d",&rows);
     printf("\nenter no.of cols : ");
-    scanf("%d",%cols);
+    scanf("%d",&cols);
     int matrix[rows][cols];
+    printf("enter matrix elements:\n");
     for(int i=0;i<rows;i++)
     {
         for(int j=0;j<cols;j++)
@@ -18,18 +21,19 @@ int main ()
             scanf("%d",&matrix[i][j]);
         }
     }
-    int (*ptr_matrix)[cols]=&matrix;
-    result=sum(ptr_matrix);
+    
+    result=sum(rows,cols,matrix);
+    printf("\n RESULT : %d",result);
     return 0;
 }
-int sum(int (*matrix)[cols])
+int sum(int rows,int cols,int (*matrix)[cols])
 {
     int result;
-    for(int i=0;i<3;i++)
+    for(int i=0;i<rows;i++)
     {
-        for(int j=0;j<3;j++)
+        for(int j=0;j<cols;j++)
         {
-            if(i%2!=0 && j%2==0)
+            if(i%2!=0 || j%2==0)
             {
                 result+=*(*(matrix+i)+j);
             }
