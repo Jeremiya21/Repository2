bdff5ae2ea7223a4e35a95ad1094f0ff96dda25c
ANORA\samsunny <samsunny@anorasolutions.com>
2023-09-05 13:32:57 +0530

    name of odd or even function hase been modified by sam

--- odd or even _sam/oddorevenfn.c
+++ odd or even _sam/oddorevenfn.c
@@ -3,16 +3,16 @@
 Program to check whether a number is odd or even
 */
 #include<stdio.h>
-void oddOreven(int a);
+void oddoreven(int a);
 int main ()
 {
     int x;
     printf("enter a number :");
     scanf("%d",&x);
-    oddOreven(x);
+    oddoreven(x);
     return 0;
 }
-void oddOreven(int a)
+void oddoreven(int a)
 {
     if(a%2==0)
     printf("%d is an even number",a);
