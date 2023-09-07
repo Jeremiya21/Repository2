7d5b4fcc52cfd44af5ce90d818cec99ac024e92d
ANORA\jeremiya <jeremiyamathew21@gmail.com>
2023-09-07 09:40:58 +0530

    Jeremy new

--- Conflict check/binary search.c
+++ Conflict check/binary search.c
@@ -1,3 +1,4 @@
+//ME? !!!
 #include <stdio.h>
 #include <stdlib.h>
 void main ()
@@ -62,10 +63,4 @@ void main ()
     }
     
 } 
-//ninak irikate oru conflict
-//enna pinne nokkallo
-//ninak onnm vere ou panim ile
-//ith thanne pani hmmm
-//njn try cheythu monee :D
-//thirich nokka
-//pani varunnund avaracha 
+
